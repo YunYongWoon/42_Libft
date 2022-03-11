561f2741b58c72fa8ef8dcf366ef64a80f58565d
Yongwoon Yun <yoyun@c1r1s3.42seoul.kr>
2022-03-11 16:08:54 +0900

    final check

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: yoyun <yoyun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 14:12:36 by yoyun             #+#    #+#             */
-/*   Updated: 2022/03/11 16:01:30 by yoyun            ###   ########.fr       */
+/*   Updated: 2022/03/11 16:08:38 by yoyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	const unsigned char	*srcs;
 
 	i = 0;
-	if (!dst || !src)
-		return (0);
 	dest = (unsigned char *)dst;
 	srcs = (const unsigned char *)src;
 	while (i < n)
